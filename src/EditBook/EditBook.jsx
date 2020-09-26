6c4eeb0648f58dae82e6331c94ee5a49c5d83315
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-26 09:31:56 +0530

    edit book completion

--- src/EditBook/EditBook.jsx
+++ src/EditBook/EditBook.jsx
@@ -2,24 +2,57 @@ import React from "react";
 import axios from "axios";
 import "./styles.css";
 export class EditBook extends React.Component {
-  state = {};
+  state = {
+    book: {},
+  };
 
-  // getBookInfo = (bookId) => {
-  //   axios
-  //     .get("http://localhost:9001/book/" + bookId)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
-
-  getPlaceHolder = (bookId) => {
+  componentDidMount = () => {
+    const bookId = this.props.match.params.id;
+    this.getBookInfo(bookId);
+  };
+  getBookInfo = (bookId) => {
     axios
       .get("http://localhost:9001/book/" + bookId)
       .then((response) => {
-        return response.data.id;
+        this.setState({
+          book: response.data,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  editBookName = (e) => {
+    const name = e.target.value;
+    this.setState({
+      book: {
+        ...this.state.book,
+        name: name,
+      },
+    });
+  };
+
+  editAuthorName = (e) => {
+    const author = e.target.value;
+    this.setState({
+      book: {
+        ...this.state.book,
+        author: author,
+      },
+    });
+  };
+
+  editBook = () => {
+    const bookId = this.state.book._id;
+    const book = this.state.book;
+
+    const { createdAt, _id, updatedAt, __v, ...restOfBook } = book;
+
+    axios
+      .put("http://localhost:9001/book/" + bookId, restOfBook)
+      .then(() => {
+        this.props.history.push(`/book/${bookId}`);
       })
       .catch((error) => {
         console.error(error);
@@ -27,13 +60,11 @@ export class EditBook extends React.Component {
   };
 
   render() {
-    const bookId = this.props.match.params.id;
-
     return (
       <div className="edit-book">
-        <input value={this.getPlaceHolder(bookId)} onChange={this.setBook} />
-        <input placeholder="Author" onChange={this.setAuthor} />
-        <button type="button" onClick={this.createBook}>
+        <input value={this.state.book.name} onChange={this.editBookName} />
+        <input value={this.state.book.author} onChange={this.editAuthorName} />
+        <button type="button" onClick={this.editBook}>
           Update
         </button>
       </div>
